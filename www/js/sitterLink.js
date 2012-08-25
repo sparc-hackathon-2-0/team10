e0eb483f7cc8145035788f501650f1d5ad77d7e1
Nick Shepherd <gene.shepherd@gmail.com>
2012-08-25 14:34:14 -0400

    Javascript is Amaaaaazing...

--- www/js/sitterLink.js
+++ www/js/sitterLink.js
@@ -1,108 +1,98 @@
-	// If you want to prevent dragging, uncomment this section
-	/*
-	function preventBehavior(e) 
-	{ 
-      e.preventDefault(); 
-    };
-	document.addEventListener("touchmove", preventBehavior, false);
-	*/
-	
-	/* If you are supporting your own protocol, the var invokeString will contain any arguments to the app launch.
-	see http://iphonedevelopertips.com/cocoa/launching-your-own-application-via-a-custom-url-scheme.html
-	for more details -jm */
-	/*
-	function handleOpenURL(url)
-	{
-		// TODO: do something with the url passed in.
-	}
-	*/
+var db = null;
 
-	function loadSitters()
-	{
-		var query = 'SELECT id, name FROM test';
-		db.executeSQL(query, onGetSitters, onSQLFail);
+// Page Events
+$('#page-sitter-search').live('pageshow', function() {
+	getSitters();
+});
+
+function init()
+{
+	try {
+		db = window.openDatabase("testdb", "1.0", "Test DB", 1000000);
+		db.transaction(bootstrapSchema, errorCB, successCB);
 
 	}
-
-	function onGetSitters(res)
+	catch(err)
 	{
-		function addSitter(row) {
-			$('.sittersResults').append('<li>
-				<a href="sitterprofile.html">
-					<div class="leftCol">
-						<h1>' + row['name'] + '</h1>
-						<h2>Location, ST</h2>
-					</div>
-					
-					<div class="rightCol">
-						<div class="previewRating">
-							 <img src="images/rateStarOn.png" class="rateStar" />
-							 <img src="images/rateStarOn.png" class="rateStar" />
-							 <img src="images/rateStarOn.png" class="rateStar" />
-							 <img src="images/rateStarHalf.png" class="rateStar" />
-							 <img src="images/rateStarOff.png" class="rateStar" />
-						</div><!-- /previewRating -->
-					</div>
-					
-					<div class="clearer">
-					</div>
-				</a>
-			</li>');
-                                        
-                                        debug.log(row);
+		alert(err);
+	}
+}
+
+
+/*
+// Generic Callbacks
+*/
+
+function errorCB(err)
+{
+	alert(err.code);
+}
+
+function successCB()
+{
+	//alert('successfully bootstrapped');
+}
+
+function queryError(err)
+{
+	alert(err.code);
+}
+
+
+/*
+// Data Calls
+*/
+
+function getSitters()
+{
+	if(db != null)
+	{
+		db.transaction(sqlGetSitters, queryError);
+	}
+}
+
+
+/*
+// DOM Manipulation Functions
+*/
+
+function domPopulateSitters(tx, results)
+{
+	var len = results.rows.length;
+
+	for(var i=0; i<len; i++)
+	{
+		try
+		{
+			$('.sitterResults').append('<li><a href="sitterprofile.html"><div class="leftCol"><h1>' + results.rows.item(i).name + '</h1><h2>Location, ST</h2></div><div class="rightCol"><div class="previewRating"><img src="images/rateStarOn.png" class="rateStar" /><img src="images/rateStarOn.png" class="rateStar" /><img src="images/rateStarOn.png" class="rateStar" /><img src="images/rateStarHalf.png" class="rateStar" /><img src="images/rateStarOff.png" class="rateStar" /></div></div><div class="clearer"></div></a></li>');
+			
 		}
-
-		res.rows.forEach(addSitter);
-		transact();
+		catch(err)
+		{
+			alert(err);
+		}
 	}
+}
 
 
-	function prepDatabase()
-	{
-		var db = new PGSQLitePlugin('sitterlink', onOpen, onFail);
-	}
+/*
+// SQL Call Functions
+*/
 
-	function bootstrapDatabase()
-	{
-		var query = '' +
-                'CREATE TABLE IF NOT EXISTS "test" (' +
-                'Id INTEGER PRIMARY KEY AUTOINCREMENT,' +
-                'name VARCHAR(100)' +
-                ');' + 
-                '';
-        db.executeSql(query, onCreateTable, onFail);
-
-	}
-
-	function addTempData()
-	{
-		var query = 'INSERT INTO "test" VALUES(1, "amy"), VALUES(2, "joan"), VALUES(3, "robin"), VALUES(4, "lilly)';
-		db.executeSql(query, onSetData, onFail);
-	}
-
-	function onSetData()
-	{
-		debug.log('data was added successfully');
-	}
-
-	function onCreateTable()
-	{
-		debug.log('table created successfully');
-	}
-
-	function onOpen()
-	{
-		debug.log('database open.')
-
-	}
-
-	function onFail(e)
-	{
-		debug.log('Database Error. Nothing to worry about. Just fix it.');
-		
-	}
-
-	/** Templates **/
+function sqlGetSitters(tx)
+{
+	tx.executeSql('SELECT * FROM sitters', [], domPopulateSitters, queryError);
+}
 
 
-    
+/*
+// Bootstrapping Functions
+*/
+
+function bootstrapSchema(tx)
+{
+	tx.executeSql('DROP TABLE IF EXISTS sitters');
+	tx.executeSql('CREATE TABLE IF NOT EXISTS sitters (id unique, name, rating)');
+	tx.executeSql('INSERT INTO sitters (id, name, rating) VALUES (1, "Lilly", 4)');
+	tx.executeSql('INSERT INTO sitters (id, name, rating) VALUES (2, "Robin", 1)');
+}
